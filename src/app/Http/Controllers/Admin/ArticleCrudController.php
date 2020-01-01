4f5474613bad3a26df8c633daa7e8cd5a1462852
Luciano Closs <lcloss@gmail.com>
2020-01-01 20:38:47 +0000

    Add top and recommended columns to Articles table

--- src/app/Http/Controllers/Admin/ArticleCrudController.php
+++ src/app/Http/Controllers/Admin/ArticleCrudController.php
@@ -211,6 +211,16 @@ class ArticleCrudController extends CrudController
                 'label' => 'Featured item',
                 'type' => 'checkbox',
             ]);
+            $this->crud->addColumn([
+                'name'  => 'top',
+                'label' => 'On top',
+                'type'  => 'check'
+            ]);
+            $this->crud->addColumn([
+                'name'  => 'recommended',
+                'label' => 'Recommended',
+                'type'  => 'check'
+            ]);
         });
     }
 
