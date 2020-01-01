98b63562e569cc8d995aeb7a6e476eadcb5b196e
Luciano Closs <lcloss@gmail.com>
2020-01-01 22:08:42 +0000

    Translate labels and publish files

---
+++ src/resources/lang/en/langnewscrud.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Backpack\NewsCRUD Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the CRUD interface for lang files.
+    | You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+
+    'meta.title'                 => 'Meta Title',
+    'meta.description'           => 'Meta Description',
+    'meta.keywords'              => 'Meta Keywords',
+
+];
