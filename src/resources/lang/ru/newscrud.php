a3a3da1f789269b987950e34d23b19a2d90ec0ee
Luciano Closs <lcloss@gmail.com>
2020-01-01 22:45:48 +0000

    Translations

---
+++ src/resources/lang/ru/newscrud.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+
+    'meta_description' => 'Описание (Description)',
+    'meta_keywords' => 'Ключевые слова (Keywords)',
+    'meta_title' => 'Название страницы (Title)',
+    
+];
