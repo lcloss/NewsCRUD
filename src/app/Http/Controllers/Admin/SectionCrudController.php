644edbd7ff0489660e3202d85998e90ba8ef574c
Luciano Closs <lcloss@gmail.com>
2020-01-08 01:32:47 +0000

    Add sectionables, remove top and recommended

---
+++ src/app/Http/Controllers/Admin/SectionCrudController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Backpack\NewsCRUD\app\Http\Controllers\Admin;
+
+use Backpack\NewsCRUD\app\Http\Requests\SectionRequest;
+use Backpack\CRUD\app\Http\Controllers\CrudController;
+use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
+
+/**
+ * Class SectionCrudController
+ * @package App\Http\Controllers\Admin
+ * @property-read CrudPanel $crud
+ */
+class SectionCrudController extends CrudController
+{
+    use \Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
+
+    public function setup()
+    {
+        $this->crud->setModel('Backpack\NewsCRUD\app\Models\Section');
+        $this->crud->setRoute(config('backpack.base.route_prefix') . '/section');
+        $this->crud->setEntityNameStrings('section', 'sections');
+    }
+
+    protected function setupListOperation()
+    {
+        // TODO: remove setFromDb() and manually define Columns, maybe Filters
+        $this->crud->setFromDb();
+    }
+
+    protected function setupCreateOperation()
+    {
+        $this->crud->setValidation(SectionRequest::class);
+
+        // TODO: remove setFromDb() and manually define Fields
+        $this->crud->setFromDb();
+    }
+
+    protected function setupUpdateOperation()
+    {
+        $this->setupCreateOperation();
+    }
+}
